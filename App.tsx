f1e00ec9bfd1b1b974154f11e40d7d50a80ae8bb
Caio18-cosenza <caiiocosenza2020@gmail.com>
2023-06-11 09:21:11 -0300

    done

--- App.tsx
+++ App.tsx
@@ -1,11 +1,67 @@
+import { useState, useCallback, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [isReady, setIsReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_600SemiBold,
+          Inter_700Bold,
+        });
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View onLayout={onLayoutRootView} style={styles.container}>
+      <Text
+        style={{
+          fontFamily: 'Inter_600SemiBold',
+          fontSize: 22,
+        }}
+      >
+        Splash Screen
+      </Text>
+      <Text
+        style={{
+          fontFamily: 'Inter_400Regular',
+          fontSize: 18,
+        }}
+      >
+        Google Fonts
+      </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
